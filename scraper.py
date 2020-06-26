5ae57aefd3a7eee89745c17fc6fac38673fa3c87
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-06-26 11:49:37 -0700

    Capture votes and links

--- scraper.py
+++ scraper.py
@@ -2,4 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get("https://news.ycombinator.com/")
-print(res.text)
+
+soup = BeautifulSoup(res.text, "html.parser")
+
+# Grab all links and votes in lists
+links = soup.select(".storylink")
+votes = soup.select(".score")
+
+print(votes[0].get("id"))
